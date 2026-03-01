cc3193d3fed83f249e9661d6420415163798c337
Roger Price <rogerprice@gmail.com>
2026-03-01 17:57:04 -0400

    Fix start.sh to run uvicorn via python3 -m for Render

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-uvicorn cleanup_api:app --host 0.0.0.0 --port $PORT
+PORT=${PORT:-10000}  # Use Render's $PORT, default to 10000 locally
+python3 -m uvicorn cleanup_api:app --host 0.0.0.0 --port $PORT
