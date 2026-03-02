e227cf8c095d3be42a28b6f62e7eb10867e699f9
Roger Price <rogerprice@gmail.com>
2026-03-01 20:20:47 -0400

    Fix Render to use python3

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Activate Render’s Python virtual environment if it exists
-if [ -f "./.venv/bin/activate" ]; then
-    source .venv/bin/activate
-fi
 
-# Use python3 explicitly
+export PATH="/opt/render/project/.venv/bin:$PATH"
+
 python3 -m uvicorn cleanup_api:app --host 0.0.0.0 --port $PORT
